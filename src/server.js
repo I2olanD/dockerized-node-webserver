c0afb850798d1c68819e7f49676164df929971d5
Roland Olah <rolandolah@icloud.com>
2021-08-19 12:04:53 +0200

    Remove unnecessary stuff

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const indexTemplate = fs.readFileSync('public/index.template.html', 'utf8');
 
-const indexTemplate = fs.readFileSync('src/index.template.html', 'utf8');
-
-// Constants
 const PORT = 80;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
+
 app.get('/', (req, res) => {
   res.send(indexTemplate);
 });
